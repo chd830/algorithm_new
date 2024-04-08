5a0fa9cdd244b6ed7e9a7fda27541a59808581cf
eunchong.shin <chd96830@gmail.com>
2024-04-08 20:48:52 +0900

    BOJ13335 개선

--- stl/BOJ13335.java
+++ stl/BOJ13335.java
@@ -5,72 +5,44 @@ import java.io.*;
 
 // https://www.acmicpc.net/status?user_id=chd830&problem_id=13335&from_mine=1
 public class BOJ13335 {
-    static class Node {
-        int val;
-        int idx;
-        Node(int val, int idx) {
-            this.val = val;
-            this.idx = idx;
-        }
-    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer token = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(token.nextToken()); // 다리를 건너는 트럭의 수
         int L = Integer.parseInt(token.nextToken()); // 다리의 길이
         int W = Integer.parseInt(token.nextToken()); // 최대하중
-        int[] arr = new int[N];
+        Queue<Integer> truck = new LinkedList<>();
         token = new StringTokenizer(br.readLine());
+        // 다리위에 올라갈 수 있는 트럭의 무게를 추가
         for(int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(token.nextToken());
+            truck.add(Integer.parseInt(token.nextToken()));
         }
 
-        Queue<Node> que = new LinkedList<>();
+        int time = 0;
         int sum = 0;
-        int cnt = 0;
-        for(int i = 0; i < N;) {
-            if(sum+arr[i] <= W) {
-                que.add(new Node(L, i));
-                sum += arr[i++];
-            }
-
-            int size = que.size();
-            cnt++;
-            for(int j = 0; j < size; j++) {
-
-                if(que.isEmpty())
-                    break;
-                Node n = que.poll();
-                if (n.val > 0) {
-                    que.add(new Node(n.val - 1, n.idx));
-                } else {
-                    sum -= arr[n.idx];
-                }
-            }
-            if(que.isEmpty())
-                continue;
-            Node tmp = que.peek();
-            if(tmp.val == 0) {
-                que.poll();
-                sum -= arr[tmp.idx];
-            }
+        // 처음 오는 트럭이 지나갈 시간(다리의 길이)
+        Queue<Integer> que = new LinkedList<>();
+        for(int i = 0; i < L; i++) {
+            que.add(0);
         }
+        // 다리 위 공간이 빌 때까지(다 지나갈 때까지)
         while(!que.isEmpty()) {
-            int size = que.size();
-            boolean flag = false;
-            for(int i = 0; i < size; i++) {
-                if(que.isEmpty())
-                    break;
-                Node n = que.poll();
-                if(n.val > 0) {
-                    flag = true;
-                    que.add(new Node(n.val-1, n.idx));
+            time++;
+            sum -= que.poll(); // 첫 트럭이 빠져나감
+            if(!truck.isEmpty()) {
+                // 지나갈 수 있는 무게일 때
+                if(truck.peek()+sum <= W) {
+                    sum += truck.peek();
+                    que.add(truck.poll());
+                }
+                // 지나갈 수 없을 때 빈 값 추가
+                else {
+                    que.add(0);
                 }
             }
-            if(flag)
-                cnt++;
+            // 트럭이 비어있다면 큐에 더이상 지나갈 수 있는 공간이 없음
         }
-
-        System.out.println(cnt+1);
+        System.out.println(time);
     }
 }
